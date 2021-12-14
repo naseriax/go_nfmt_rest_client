2b2f7b9ae5e3e32c00da62ad72d960084b93fce5
Naseredin Aramnejad <naseredin.aramnejad@gmail.com>
2021-12-14 15:58:09 +0300

    Add Controlled Concurrency

--- main.go
+++ main.go
@@ -1,56 +1,122 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"sync"
 	"time"
 )
 
-func pprint(content []map[string]interface{}) {
-	for ind, item := range content {
-		fmt.Printf("%v:\n", ind)
-		for k, v := range item {
-			fmt.Printf("%v : %v\n", k, v)
-		}
-	}
-}
-
-func updateSw(agent RestAgent) {
-
+func getNEs(agent RestAgent) map[string][]map[string]interface{} {
 	var allNEsJson map[string][]map[string]interface{}
 	allNEsRaw := agent.HttpGet("/data/swim/neSoftware", map[string]string{"Range": "items=0-1999"})
 	json.Unmarshal([]byte(allNEsRaw), &allNEsJson)
-	var neList []map[string]interface{}
 
+	return allNEsJson
+}
+
+func neList(allNEsJson map[string][]map[string]interface{}) []map[string]interface{} {
+	var neList []map[string]interface{}
 	for _, ne := range allNEsJson["items"] {
 		if ne["neType"] == "1830PSS-PHN" {
 			neList = append(neList, ne)
 		}
 	}
-	for _, ne := range neList {
-		fmt.Printf("Reading the software version from %v.\n", ne["neLabel"])
-		_ = agent.HttpGet(fmt.Sprintf("/swim/neSwStatus?neLabel=%v&neType=--", ne["neLabel"]), map[string]string{})
-		time.Sleep(1 * time.Second)
-	}
+	return neList
 }
 
-func GetRamanConnections(agent RestAgent) {
+func updateSw(agent RestAgent) {
 
-	rawOtsData := agent.HttpGet("/data/npr/physicalConns", map[string]string{})
-	_, listJson := GeneralJsonDecoder(rawOtsData)
-	var otslist []map[string]interface{}
-	for _, phycon := range listJson {
-		if phycon["wdmConnectionType"] == "WdmPortType_ots" {
-			otslist = append(otslist, phycon)
+	allNEsJson := getNEs(agent)
+
+	nelist := neList(allNEsJson)
+
+	var wg sync.WaitGroup
+	workers := 5
+	j := 0
+	for _, ne := range nelist {
+		if j >= workers {
+			fmt.Println("Waiting for an idle worker.")
+			wg.Wait()
 		}
+
+		wg.Add(1)
+		j += 1
+
+		go func(ne map[string]interface{}) {
+			fmt.Printf("Reading the software version from %v.\n", ne["neLabel"])
+			_ = agent.HttpGet(fmt.Sprintf("/swim/neSwStatus?neLabel=%v&neType=--", ne["neLabel"]), nil)
+			wg.Done()
+			j -= 1
+		}(ne)
 	}
-	pprint(otslist)
+	wg.Wait()
+
+	output := [][]string{
+		{
+			"No",
+			"NE Name",
+			"Software Version",
+		},
+	}
+	c := 1
+	allNEsJson = getNEs(agent)
+	neVersion := ""
+
+	for _, ne := range allNEsJson["items"] {
+		neName := ne["neLabel"]
+
+		if ne["primaryCurStatus"] == "ACTIVATED" {
+			neVersion = ne["primarySWVersion"].(string)
+		} else if ne["secondaryCurStatus"] == "ACTIVATED" {
+			neVersion = ne["secondarySWVersion"].(string)
+		} else {
+			neVersion = "UNKNOWN"
+		}
+
+		rowToAdd := []string{
+			fmt.Sprintf("%v", c),
+			fmt.Sprintf("%v", neName),
+			fmt.Sprintf("%v", neVersion),
+		}
+		output = append(output, rowToAdd)
+	}
+
+	err := exportFile(output)
+	if err == nil {
+		log.Println("SUCCESS: NE Software report file has been exported!")
+	} else {
+		panic(err)
+	}
+
+}
+
+func timeCalculator() int64 {
+	return time.Now().Unix()
+}
+
+func exportFile(output [][]string) error {
+	ts := fmt.Sprintf("%v", timeCalculator())
+	csvFile, err := os.Create(fmt.Sprintf("output_%v.csv", ts))
+	if err != nil {
+		return err
+	} else {
+		csvwriter := csv.NewWriter(csvFile)
+		for _, empRow := range output {
+			_ = csvwriter.Write(empRow)
+		}
+		csvwriter.Flush()
+	}
+	return nil
 }
 
 func main() {
-	ipaddr := "172.29.4.72"
-	uname := "admin"
-	passw := "Changeme_1@#"
+	ipaddr := "1.2.3.4"
+	uname := "alcatel"
+	passw := "password"
 	restAgent := Init(ipaddr, uname, passw)
 	defer restAgent.NfmtDeauth()
 
